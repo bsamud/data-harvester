b1347a43c2f6b7f185848fa4cebfebb5a0eec649
Bharat Samudrala <bharat.samudrala@example.com>
2025-01-04 14:43:27 -0500

    design plugin architecture

---
+++ plugins/README.md
@@ -0,0 +1,8 @@
+# Plugins
+
+Plugin architecture for dataHarvest.
+
+## Creating Plugins
+1. Extend Plugin base class
+2. Implement initialize() and process()
+3. Add plugin.yaml configuration
---
+++ plugins/core/plugin_base.py
@@ -0,0 +1,10 @@
+class Plugin:
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+
+    def initialize(self):
+        raise NotImplementedError
+
+    def process(self, data):
+        raise NotImplementedError
